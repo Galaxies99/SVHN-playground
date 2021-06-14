8f877fb0d24b9b0083b920d35ab38635819d478e
Tony Fang <galaxies@vip.qq.com>
2021-06-14 14:46:03 +0800

    upd hog feature, final

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -10,14 +10,14 @@ class LogisticRegression(nn.Module):
     def __init__(self, **kwargs):
         super(LogisticRegression, self).__init__()
         dim = kwargs.get('dim', 32 * 32 * 3)
-        self.beta = nn.Parameter(torch.FloatTensor(dim))
+        self.final = nn.Linear(dim, 1, bias = False)
     
     def forward(self, x):
-        if x.dim == 3:
+        if x.dim() == 3:
             x = rearrange(x, 'b h w -> b (h w)')
-        elif x.dim == 4:
+        elif x.dim() == 4:
             x = rearrange(x, 'b c h w -> b (c h w)')
-        return torch.sigmoid(torch.matmul(x, self.beta.view(-1, 1)).view(-1))
+        return torch.sigmoid(self.final(x).view(-1))
     
     def loss(self, pred, gt):
         return (- pred * gt + torch.log(1.0 + torch.exp(pred))).mean()
